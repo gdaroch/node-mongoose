c11092760dd889e31d5d5a4c8180be3922a36a35
Gonzalo Cuadra <gonzalocuadra@protonmail.com>
2020-04-07 17:17:43 -0400

    Mongoose Part 2

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
   console.log('Connected correctly to server');
 
-  let newDish = Dishes({
+  Dishes.create({
     name: 'Uthappiuzza',
     description: 'test'
-  });
-
-  newDish.save()
+  })
   .then((dish) => {
     console.log(dish);
 
